2d39b3f20504a348330781a8fc562966ea6777c3
charan_teja <jazzcharan758@gmail.com>
2026-03-05 21:19:03 +0530

    Add files via upload

---
+++ remove_duplicates.c
@@ -0,0 +1,40 @@
+
+#include <stdio.h>
+
+int main() {
+    int a[100],i,j,k,n;
+    printf("enter the number of elements:");
+    scanf("%d",&n);
+    printf("\nenter the elements into an array:");
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("\nbefore removing the duplicate the array is:");
+     for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]==a[j])
+            {
+                for(k=j;k<n-1;k++)
+                {
+                    a[k]=a[k+1];
+                }
+                n--;
+                j--;
+            }
+        }
+    }
+    printf("\narray after removing duplicates:");
+    for(i=0;i<n;i++)
+    {
+        printf("%d",a[i]);
+    }
+
+    return 0;
+}
